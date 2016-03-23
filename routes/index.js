c47809e04b1c0d5037f859478739f162402a6a99
lvasansa <ivasansa@gmail.com>
2016-03-23 17:28:00 +0000

    h

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/c9', function(req, res, next) {
-  res.render("/public/indexc9.html");
+//   res.render(public/indexc9.html);
 });
 
 module.exports = router;
