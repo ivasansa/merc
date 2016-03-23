51b833d18c4fd8ed3062b588546e58c900b76d06
lvasansa <ivasansa@gmail.com>
2016-03-23 18:25:28 +0000

    h

--- public/javascripts/boot.js
+++ public/javascripts/boot.js
@@ -1,5 +1,6 @@
 (function(app) {
   document.addEventListener('DOMContentLoaded', function() {
+    ng.platform.browser.bootstrap(app.AppComponentDos);
     ng.platform.browser.bootstrap(app.AppComponent);
   });
 })(window.app || (window.app = {}));
--- public/javascripts/component.js
+++ public/javascripts/component.js
@@ -9,4 +9,15 @@
                 this.nom = "Sergi"
             }
         });
+        
+    app.AppComponentDos =
+        ng.core.Component({
+            selector: 'appProva2',
+            template: '<h1>Primera aplicació amb Angular2, {{nom}}</h1>'
+        })
+        .Class({
+            constructor: function () {
+                this.nom = "Ivan"
+            }
+        });
 })(window.app || (window.app = {}));
