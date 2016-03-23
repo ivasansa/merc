809470a0d1a81f2de74b8d3ba2f091ca5fb8717e
lvasansa <ivasansa@gmail.com>
2016-03-23 17:55:13 +0000

    h

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use('/', routes);
 
 // catch 404 and forward to error handler
