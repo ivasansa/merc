20fb7d4612593a7766282d5c45fd2ab03b22f714
lvasansa <ivasansa@gmail.com>
2016-05-02 15:03:29 +0000

    m

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
@@ -20,6 +20,26 @@ app.use(express.static(path.join(__dirname, './public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+    // default to a 'localhost' configuration:
+    var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
+    // if OPENSHIFT env variables are present, use the available connection info:
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+      connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+      process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+      process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+      process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+      process.env.OPENSHIFT_APP_NAME;
+    }
+
+MongoClient.connect('mongodb://'+connection_string, function(err, db) {
+  if(err) throw err;
+  var collection = db.collection('books').find().limit(10).toArray(function(err, docs) {
+    console.dir(docs);
+    db.close();
+  })
+})
+
 
 app.use('/', routes);
 
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var AppContainer = function () {
     //  Set the environment variables we need.
     self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP ;
     self.port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT ;
+    
+    
 
     if (typeof self.ipaddress === "undefined") {
       //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
