472310d81165969e75b700a92d898def9644f50c
lvasansa <ivasansa@gmail.com>
2016-05-03 18:31:09 +0000

    angular

--- public/app/component.js
+++ public/app/component.js
@@ -7,6 +7,34 @@
         .Class({
             constructor: function () {
                 this.nom = "Sergi"
+                this.dragging = false;
+            },
+            handleDragEnter: function(ev) {
+                ev.preventDefault();
+                this.dragging = true;
+                console.log(this.dragging);
+            },
+            handleDragLeave: function(ev) {
+                ev.preventDefault();
+                this.dragging = false;
+                console.log(this.dragging);
+            },
+            handleAllowDrop: function(ev) {
+                ev.preventDefault();
+                console.log("allowDrop");
+            },
+            handleDrag: function(ev) {
+                ev.dataTransfer.setData("text", ev.target.id);
+                console.log("drag");
+            },
+            handleDrop: function(ev) {
+                console.log("drop");
+                this.dragging = false;
+                ev.preventDefault();
+                var data = ev.dataTransfer.getData("text");
+                console.log(data);
+                ev.target.appendChild(document.getElementById(data));
             }
         });
+        
 })(window.app || (window.app = {}));
